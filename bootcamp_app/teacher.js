ea15dfa8c740b20a219098dabb4176345d1e7e28
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-06-12 13:22:52 -0600

    connecting to db and importing sql data through js

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -29,15 +29,17 @@ const pool = new Pool({
 
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
 `)
 .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-    })
+    res.rows.forEach(query =>
+      console.log(`${query.cohort}: ${query.teacher}`)
+    );
   }).catch(err => console.error('query error', err.stack));
 
